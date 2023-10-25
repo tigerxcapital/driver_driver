396a0e2cdb3a2369304a7a7f846a313c9dcc8d1c
jeronimo0007 <jeronimo.alvescardoso@gmail.com>
2023-10-25 17:54:41 -0300

    primeiro commit

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,44 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audioplayers/web/audioplayers_web.dart';
+import 'package:cashfree_pg/cashfree_pg_web.dart';
+import 'package:connectivity_plus_web/connectivity_plus_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:location_web/location_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:share_plus_web/share_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioplayersPlugin.registerWith(registrar);
+  CashfreePgWeb.registerWith(registrar);
+  ConnectivityPlusPlugin.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
+  SharePlusPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,47 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering, depend_on_referenced_packages
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:audioplayers/web/audioplayers_web.dart';
+import 'package:cashfree_pg/cashfree_pg_web.dart';
+import 'package:connectivity_plus_web/connectivity_plus_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:location_web/location_web.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:share_plus_web/share_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  AudioplayersPlugin.registerWith(registrar);
+  CashfreePgWeb.registerWith(registrar);
+  ConnectivityPlusPlugin.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
+  PackageInfoPlusLinuxPlugin.registerWith();
+  PackageInfoPlusWindowsPlugin.registerWith();
+  SharePlusPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
